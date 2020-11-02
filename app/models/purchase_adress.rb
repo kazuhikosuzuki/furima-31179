4aae59396fecfc53bf7af84a61e02649ca822307
kazuhikosuzuki <72254144+kazuhikosuzuki@users.noreply.github.com>
2020-11-02 21:19:10 +0900

    バリデーションの設定

--- app/models/purchase_adress.rb
+++ app/models/purchase_adress.rb
@@ -3,11 +3,11 @@ class PurchaseAdress
   attr_accessor :token,:postal_code,:prefecture_id,:city,:house_number,:building_name,:phone_number,:good_id,:user_id
 
 with_options presence: true do
-  validates :token
-  validates :prefecture_id
+  validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  validates :prefecture_id,numericality: { other_than: 1 }
   validates :city
   validates :house_number
-  validates :phone_number
+  validates :phone_number, format: {with: /\A[0-9]{11}+\z/ ,message:""},
 end
 
   def save  
